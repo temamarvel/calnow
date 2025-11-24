b19ffee14d1e646da7559665ea5e534a9396f9dc
temamarvel <tema.denisoff@gmail.com>
2025-11-25 00:14:55 +0400

    try to show basal calories data correctly

--- calnow/HealthKitManager.swift
+++ calnow/HealthKitManager.swift
@@ -343,8 +343,8 @@ final class HealthKitManager: ObservableObject, HealthKitServicing {
     
     func basalEnergyPoints(for period: BasalChartPeriod) async throws -> [BasalEnergyPoint] {
         let calendar = Calendar.current
-        let end = calendar.startOfDay(for: Date())
-        let start = calendar.date(byAdding: .day, value: -(period.days - 1), to: end)!
+        let end = Date()
+        let start = calendar.date(byAdding: .day, value: -period.days, to: calendar.startOfDay(for: end))!
         let interval = DateInterval(start: start, end: end)
 
         // здесь можно использовать твой dailyBuckets(for: .basalEnergyBurned, in: interval)
