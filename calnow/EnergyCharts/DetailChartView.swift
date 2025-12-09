4b0f96b4cfd6a85dc2cc4e4b1c144250e99177bb
temamarvel <tema.denisoff@gmail.com>
2025-12-09 23:39:38 +0400

    wip annotation tooltip

--- calnow/EnergyCharts/DetailChartView.swift
+++ calnow/EnergyCharts/DetailChartView.swift
@@ -16,6 +16,16 @@ struct DetailChartView: View {
     
     var body: some View {
         Chart {
+            // Вертикальная линия для выбранного дня
+            if let selectedPoint {
+                RuleMark(
+                    x: .value("Дата", selectedPoint.date, unit: .day)
+                )
+                .foregroundStyle(.secondary)
+                .lineStyle(StrokeStyle(lineWidth: 1))
+            }
+            
+            
             ForEach(points) { point in
                 BarMark(
                     x: .value("Дата", point.date, unit: unit),
@@ -24,28 +34,87 @@ struct DetailChartView: View {
                 .foregroundStyle(selectedPoint?.id == point.id ? .orange : .blue)
             }
         }
+        .chartPlotStyle { $0.padding(.top, 100) }
         .chartOverlay { proxy in
             GeometryReader { geo in
-                Rectangle()
-                    .fill(.clear)
-                    .contentShape(Rectangle())
-                    .gesture(
-                        SpatialTapGesture()
-                            .onEnded { value in
-                                let location = value.location
-                                
-                                if let date: Date = proxy.value(atX: location.x) {
-                                    if let nearest = points.min(by: {
-                                        abs($0.date.timeIntervalSince(date)) <
-                                        abs($1.date.timeIntervalSince(date))
-                                    }) {
-                                        selectedPoint = nearest
+                ZStack{
+                    Rectangle()
+                        .fill(.clear)
+                        .contentShape(Rectangle())
+                        .gesture(
+                            SpatialTapGesture()
+                                .onEnded { value in
+                                    let location = value.location
+                                    
+                                    if let date: Date = proxy.value(atX: location.x) {
+                                        if let nearest = points.min(by: {
+                                            abs($0.date.timeIntervalSince(date)) <
+                                                abs($1.date.timeIntervalSince(date))
+                                        }) {
+                                            selectedPoint = nearest
+                                        }
                                     }
                                 }
-                            }
-                    )
+                            
+                            
+                        )
+                    
+                    if let selectedPoint,
+                       let plotFrame = proxy.plotFrame,
+                       let xPos = proxy.position(
+                        forX: selectedPoint.date
+                       ) {
+                        
+                        let frame = geo[plotFrame]
+                        // x внутри overlay (учитываем отступ plotFrame)
+                        let tooltipX = xPos + frame.origin.x
+                        // y чуть выше верхней границы plotFrame
+                        let tooltipY = frame.origin.y - 12
+                        
+                        tooltip(for: selectedPoint)
+                            //.fixedSize()          // пусть сам подбирает размер
+                            .position(x: tooltipX, y: max(tooltipY, 0 + 32)) // чтобы не уехал за край
+                    }
+                }
             }
         }
+    }
+    
+    @ViewBuilder
+    private func tooltip(for point: EnergyPoint) -> some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text("ВСЕГО")
+                .font(.caption2)
+                .foregroundStyle(.secondary)
+            
+            HStack(alignment: .firstTextBaseline, spacing: 4) {
+                Text(
+                    Int(point.kcal),
+                    format: .number.grouping(.automatic) // 2 163
+                )
+                .font(.system(size: 28, weight: .semibold, design: .rounded))
+                
+                Text("ккал")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+            }
+            
+            Text(
+                point.date,
+                format: .dateTime
+                    .day()
+                    .month(.abbreviated)
+                    .year()
+            )
+            .font(.caption2)
+            .foregroundStyle(.secondary)
+        }
+        .padding(.horizontal, 10)
+        .padding(.vertical, 8)
+        .background(
+            RoundedRectangle(cornerRadius: 12, style: .continuous)
+                .fill(.regularMaterial) // на тёмной будет почти как чёрный
+        )
     }
 }
 
